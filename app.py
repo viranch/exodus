f1cb2d9f70da8ef724c418949cb69a0dd0533891
Viranch Mehta <email@viranch.me>
2017-02-19 01:04:55 +0530

    Auto reload flask

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ def play():
     return render_template('player.html', url=url, item=args)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True, use_reloader=True)
